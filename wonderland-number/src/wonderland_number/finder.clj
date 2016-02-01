b7609f845d3a0333cc37dcb818bf56a268433d4d
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2016-02-01 14:15:54 +0100

    Find wonderland number.

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,22 @@
 (ns wonderland-number.finder)
 
+(defn- digits [number]
+  (sort (str number)))
+
+(defn- having-correct-digits [number]
+  (let [number-digits (digits number)]
+    (and
+      (= number-digits (digits (* 2 number)))
+      (= number-digits (digits (* 3 number)))
+      (= number-digits (digits (* 4 number)))
+      (= number-digits (digits (* 5 number)))
+      (= number-digits (digits (* 6 number)))
+    )
+  )
+)
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (first
+    (filter
+	     having-correct-digits
+	      (range 100000 1000000))))
