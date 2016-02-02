c000193decd0d03e9b5686e4344b9a2cbd312022
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2016-02-02 13:29:37 +0100

    Refactored with thread macro.

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -3,20 +3,13 @@
 (defn- digits [number]
   (sort (str number)))
 
-(defn- having-correct-digits [number]
-  (let [number-digits (digits number)]
-    (and
-      (= number-digits (digits (* 2 number)))
-      (= number-digits (digits (* 3 number)))
-      (= number-digits (digits (* 4 number)))
-      (= number-digits (digits (* 5 number)))
-      (= number-digits (digits (* 6 number)))
-    )
-  )
-)
+(defn- correct-digits? [number]
+  (->> (map #(* number %) (range 2 7))
+       (map digits)
+       (every? #(= (digits number) %))))
 
 (defn wonderland-number []
   (first
     (filter
-	     having-correct-digits
+	     correct-digits?
 	      (range 100000 1000000))))
